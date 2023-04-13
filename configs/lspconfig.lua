ee51e1b020a9c8821621969eab44e0c3647d433c
Bagwan Shahabaz <>
2023-04-13 16:34:43 +0200

    settings for lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -25,5 +25,90 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
+lspconfig.clangd.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--suggest-missing-includes",
+      "--completion-style=bundled",
+      "--cross-file-rename",
+      "--header-insertion=iwyu",
+    },
+    root_dir = {
+      root_pattern = {
+        '.clangd',
+        '.clang-tidy',
+        '.clang-format',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac',
+        '.git'
+      },
+    },
+
+    filetypes = {
+     "c",
+     "cpp",
+     "objc",
+     "objcpp",
+     "cuda",
+     "proto"
+    },
+    single_file_support = true,
+    init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+    clangdFileStatus = true
+    },
+    flags = { debounce_text_changes = 150 }
+  }
+
+}
+
+lspconfig.rust_analyzer.setup {
+  -- standalone file support
+  -- setting it to false may improve startup time
+  standalone = true,
+  -- options to pass to nvim-lspconfig
+  -- i.e. the arguments to require("lspconfig").clangd.setup({})
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    cmd = {"rust-analyzer" },
+    ["rust-analyzer"] = {
+      cargo = {
+        autoReload = true
+      }
+    },
+    filetypes = { "rust" },
+    root_dir = {
+      root_pattern = {
+        "Cargo.toml", "rust-project.json"
+      }
+    }
+  }
+}
+
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+
+  settings = {
+    Lua = {
+      format = {
+        enable = true,
+        -- Put format options here
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          quote_style = "double",
+          max_line_length = "unset"
+        },
+      },
+    },
+  },
+}
