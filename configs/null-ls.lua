0ef86c2b90d831b80d9e75b8a2796cdb825c418b
Bagwan Shahabaz <>
2023-05-15 15:51:51 +0200

    add autoformat on save feature

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -13,9 +13,43 @@ local sources = {
 
   -- cpp
   b.formatting.clang_format,
+
+  --cmake
+  b.formatting.cmake_format,
+  b.diagnostic.cmake_lint,
+
+  --markdown
+  b.formatting.markdownlint,
+  b.diagnostic.markdownlint,
+
+  --json
+  b.formatting.fixjson,
+
+  --xml
+  b.formatting.xmlformat,
+
+
+
 }
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
+        group = augroup,
+        buffer = bufnr,
+      }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
