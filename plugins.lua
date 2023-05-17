0602ce7e460392da6f2ee078b3904688006d26d8
Bagwan Shahabaz <>
2023-05-17 16:33:11 +0200

    new plugins

--- plugins.lua
+++ plugins.lua
@@ -1,26 +1,179 @@
 local overrides = require "custom.configs.overrides"
 
----@type NvPluginSpec[]
 local plugins = {
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available
   {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    cond = vim.fn.executable "make" == 1,
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = vim.fn.executable "make" == 1,
+    },
+    opts = overrides.telescope,
   },
 
   {
-    "nvim-telescope/telescope-file-browser.nvim",
+    "NvChad/ui",
+    branch = "v2.0",
+    opts = overrides.ui,
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "nvim-tree", "Trouble", "lazy", "mason" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+    config = function(_, opts)
+      require("mini.indentscope").setup(opts)
+    end,
+  },
+
+  { "tpope/vim-repeat", event = "VeryLazy" },
+
+  {
+    "rcarriga/nvim-notify",
     lazy = false,
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss { silent = true, pending = true }
+        end,
+        desc = "Dismiss all Notifications",
+      },
+      {
+        "<leader>uh",
+        function()
+          require("notify").history { silent = true, pending = true }
+        end,
+        desc = "Old Notifications",
+      },
+    },
+    -- opts = {
+    --   timeout = 3000,
+    --   background_colour = "#000000",
+    --   max_height = function()
+    --     return math.floor(vim.o.lines * 0.75)
+    --   end,
+    --   max_width = function()
+    --     return math.floor(vim.o.columns * 0.75)
+    --   end,
+    -- },
   },
 
   {
-    "nvim-telescope/telescope-ui-select.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    keys = {
+      {
+        "<leader>sq",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore Session",
+      },
+      {
+        "<leader>lq",
+        function()
+          require("persistence").load { last = true }
+        end,
+        desc = "Restore Last Session",
+      },
+      {
+        "<leader>dq",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Don't Save Current Session",
+      },
+    },
   },
+
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require "plugins.configs.cmp"
+      require "custom.configs.cmp"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- better vim.ui
+  {
+    "stevearc/dressing.nvim",
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load { plugins = { "dressing.nvim" } }
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load { plugins = { "dressing.nvim" } }
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
+  -- better diagnostics list and others
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = true },
+  },
+
+  {
+    "RRethy/vim-illuminate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = { delay = 200 },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
+    end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
+    },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-file-browser.nvim" },
+  },
+
+  -- {
+  --   "nvim-telescope/telescope-ui-select.nvim",
+  --   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  -- },
 
   {
     "ggandor/leap.nvim",
