e49180e36f04f785ea7c0350198f33c577897ba9
Bagwan Shahabaz <>
2023-06-07 11:44:10 +0200

    small fix

--- mappings.lua
+++ mappings.lua
@@ -20,7 +20,6 @@ M.general = {
 }
 
 M.nvimtree = {
-  plugin = true,
   n = {
     -- toggle
     ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
@@ -31,7 +30,6 @@ M.nvimtree = {
 }
 
 M.cmaketools = {
-  plugin = true,
   n = {
     ["<leader>cg"] = { "<cmd> CMakeGenerate <CR>", "Cmake Generate" },
     ["<leader>cx"] = { "<cmd> CMakeGenerate! <CR>", "Clean and generate" },
@@ -51,7 +49,6 @@ M.cmaketools = {
 }
 
 M.lspconfig = {
-  plugin = true,
   n = {
 
     ["gd"] = { "<cmd>Telescope lsp_definitions<cr>", "Goto Definition" },
@@ -68,7 +65,6 @@ M.lspconfig = {
 }
 
 M.telescope = {
-  plugin = true,
   n = {
     ["<leader>fk"] = {
       "<cmd> Telescope keymaps <CR>",
@@ -81,8 +77,6 @@ M.telescope = {
 }
 
 M.nvterm = {
-  plugin = true,
-
   t = {
     -- toggle in terminal mode
     ["<leader>i"] = {
