2b16896dc29c6c4c9895147a91876d595dccb9f0
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2024-02-12 14:06:33 +0100

    move from null-ls to none-ls

--- plugins.lua
+++ plugins.lua
@@ -236,7 +236,7 @@ local plugins = {
     dependencies = {
       -- format & linting
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         config = function()
           require("custom.configs.null-ls")
         end,
