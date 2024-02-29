00fffa5233758da75ba64a4160c1ad079b44f89b
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2024-02-29 14:56:51 +0100

    remove flit

--- plugins.lua
+++ plugins.lua
@@ -159,18 +159,6 @@ local plugins = {
       { "[[", desc = "Prev Reference" },
     },
   },
-  {
-    "ggandor/flit.nvim",
-    keys = function()
-      ---@type LazyKeys[]
-      local ret = {}
-      for _, key in ipairs({ "f", "F", "t", "T" }) do
-        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-      end
-      return ret
-    end,
-    opts = { labeled_modes = "nx" },
-  }, -- Override plugin definition options
 
   {
     "kylechui/nvim-surround", -- Surround
