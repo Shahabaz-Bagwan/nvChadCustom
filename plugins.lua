0c4f393c016831b38fce8e57499ab3757a59e6f3
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2024-06-25 11:04:33 +0200

    format with lua-ls and remove unavailable plugins

--- plugins.lua
+++ plugins.lua
@@ -1,266 +1,261 @@
 local overrides = require("custom.configs.overrides")
 
 local plugins = {
-  -- Lazy
-  {
-    "crnvl96/lazydocker.nvim",
-    event = "VeryLazy",
-    opts = {}, -- automatically calls `require("lazydocker").setup()`
-    init = function()
-      require("core.utils").load_mappings("lazydocker")
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-  },
-  {
-    "https://codeberg.org/esensar/nvim-dev-container",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    cmd = { "DevcontainerStart", "DevcontainerAttach", "DevcontainerEditNearestConfig" },
-    config = function()
-      require("devcontainer").setup({})
-    end,
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    event = "VeryLazy",
-    init = function()
-      require("core.utils").load_mappings("lazygit")
-    end,
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "VeryLazy",
-  },
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+	-- Lazy
+	{
+		"crnvl96/lazydocker.nvim",
+		event = "VeryLazy",
+		opts = {}, -- automatically calls `require("lazydocker").setup()`
+		init = function()
+			require("core.utils").load_mappings("lazydocker")
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		"https://codeberg.org/esensar/nvim-dev-container",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		cmd = { "DevcontainerStart", "DevcontainerAttach", "DevcontainerEditNearestConfig" },
+		config = function()
+			require("devcontainer").setup({})
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "VeryLazy",
+		init = function()
+			require("core.utils").load_mappings("lazygit")
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "VeryLazy",
+	},
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
     -- stylua: ignore
     keys = {
       { "<leader>rs",  function() require("persistence").load() end,                desc = "Restore Session" },
       { "<leader>rls", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
     },
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-  },
-  {
-    "nvim-dap-repl-highlights",
-  },
-  {
-    "rcarriga/cmp-dap",
-    config = function()
-      require("cmp").setup({
-        enabled = function()
-          return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or
-          require("cmp_dap").is_dap_buffer()
-        end,
-      })
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	{
+		"rcarriga/cmp-dap",
+		config = function()
+			require("cmp").setup({
+				enabled = function()
+					return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+				end,
+			})
 
-      require("cmp").setup.filetype(
-      { "dap-repl", "dapui_watches", "dapui_hover" }, {
-        sources = {
-          { name = "dap" },
-        },
-      })
-    end,
-  },
+			require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+				sources = {
+					{ name = "dap" },
+				},
+			})
+		end,
+	},
 
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    opts = {
-      handlers = {},
-    },
-  },
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
 
-  {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings("dap")
-      require("custom.configs.dap")
-    end,
-  },
+	{
+		"mfussenegger/nvim-dap",
+		config = function(_, _)
+			require("core.utils").load_mappings("dap")
+			require("custom.configs.dap")
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "HUAHUAI23/telescope-dapzzzz",
-        lazy = false,
-      },
-    },
-    opts = overrides.telescope,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"HUAHUAI23/telescope-dapzzzz",
+				lazy = false,
+			},
+		},
+		opts = overrides.telescope,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local opts = require("plugins.configs.cmp")
-      local other_opts = require("custom.configs.cmp")
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local opts = require("plugins.configs.cmp")
+			local other_opts = require("custom.configs.cmp")
 
-      local merge_tb = vim.tbl_deep_extend
-      opts = merge_tb("force", opts, other_opts)
-      require("cmp").setup(opts)
-    end,
-  },
+			local merge_tb = vim.tbl_deep_extend
+			opts = merge_tb("force", opts, other_opts)
+			require("cmp").setup(opts)
+		end,
+	},
 
-  {
-    "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = { "BufReadPre", "BufNewFile" },
-    opts = { symbol = "│", options = { try_as_border = true } },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "nvim-tree", "Trouble", "lazy", "mason" },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-    config = function(_, opts)
-      require("mini.indentscope").setup(opts)
-    end,
-  },
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		event = { "BufReadPre", "BufNewFile" },
+		opts = { symbol = "│", options = { try_as_border = true } },
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "nvim-tree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+		config = function(_, opts)
+			require("mini.indentscope").setup(opts)
+		end,
+	},
 
-  { "tpope/vim-repeat",                event = "VeryLazy" },
+	{ "tpope/vim-repeat", event = "VeryLazy" },
 
-  {
-    "RRethy/vim-illuminate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = { delay = 200 },
-    config = function(_, opts)
-      require("illuminate").configure(opts)
+	{
+		"RRethy/vim-illuminate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = { delay = 200 },
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
-      local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, {
-          desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference",
-          buffer = buffer,
-        })
-      end
+			local function map(key, dir, buffer)
+				vim.keymap.set("n", key, function()
+					require("illuminate")["goto_" .. dir .. "_reference"](false)
+				end, {
+					desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference",
+					buffer = buffer,
+				})
+			end
 
-      map("]]", "next")
-      map("[[", "prev")
+			map("]]", "next")
+			map("[[", "prev")
 
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map("]]", "next", buffer)
-          map("[[", "prev", buffer)
-        end,
-      })
-    end,
-    keys = {
-      { "]]", desc = "Next Reference" },
-      { "[[", desc = "Prev Reference" },
-    },
-  },
+			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buffer = vim.api.nvim_get_current_buf()
+					map("]]", "next", buffer)
+					map("[[", "prev", buffer)
+				end,
+			})
+		end,
+		keys = {
+			{ "]]", desc = "Next Reference" },
+			{ "[[", desc = "Prev Reference" },
+		},
+	},
 
-  {
-    "kylechui/nvim-surround", -- Surround
-    lazy = false,
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
+	{
+		"kylechui/nvim-surround", -- Surround
+		lazy = false,
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 
-  {
-    "p00f/clangd_extensions.nvim", -- clangd extension, some good stuff
-    ft = { "c", "cpp" },
-    -- opts = overrides.clangd_extensions,
-    -- config = function()
-    --   require "custom.configs.clangd_extensions"
-    -- end,
-  },
+	{
+		"p00f/clangd_extensions.nvim", -- clangd extension, some good stuff
+		ft = { "c", "cpp" },
+		-- opts = overrides.clangd_extensions,
+		-- config = function()
+		--   require "custom.configs.clangd_extensions"
+		-- end,
+	},
 
-  {
-    "simrat39/rust-tools.nvim", -- rust, rust, it's rust!
-    ft = { "rust" },
-    -- opts = overrides.rust_tools,
-    -- config = function()
-    --   require "custom.configs.rust_tools"
-    -- end,
-  }, -- better diagnostics list and others
+	{
+		"simrat39/rust-tools.nvim", -- rust, rust, it's rust!
+		ft = { "rust" },
+		-- opts = overrides.rust_tools,
+		-- config = function()
+		--   require "custom.configs.rust_tools"
+		-- end,
+	}, -- better diagnostics list and others
 
-  {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-  },
+	{
+		"folke/trouble.nvim",
+		cmd = { "TroubleToggle", "Trouble" },
+		opts = { use_diagnostic_signs = true },
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "nvimtools/none-ls.nvim",
-        config = function()
-          require("custom.configs.null-ls")
-        end,
-      },
-    },
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end, -- Override to setup mason-lspconfig
-  },   -- override plugin configs
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"nvimtools/none-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	}, -- override plugin configs
 
-  { "williamboman/mason.nvim",         opts = overrides.mason },
+	{ "williamboman/mason.nvim", opts = overrides.mason },
 
-  { "nvim-treesitter/nvim-treesitter", opts = overrides.treesitter },
+	{ "nvim-treesitter/nvim-treesitter", opts = overrides.treesitter },
 
-  { "lewis6991/gitsigns.nvim",         opts = overrides.gitsigns },
+	{ "lewis6991/gitsigns.nvim", opts = overrides.gitsigns },
 
-  { "nvim-tree/nvim-tree.lua",         lazy = false,               opts = overrides.nvimtree },
+	{ "nvim-tree/nvim-tree.lua", lazy = false, opts = overrides.nvimtree },
 
-  {
-    "NvChad/nvterm",
-    enabled = false,
-  },
-  {
-    "Civitasv/cmake-tools.nvim",
-    keys = { "<leader>c" },
+	{
+		"NvChad/nvterm",
+		enabled = false,
+	},
+	{
+		"Civitasv/cmake-tools.nvim",
+		keys = { "<leader>c" },
 
-    init = function()
-      require("core.utils").load_mappings("cmaketools")
-    end,
-    opts = overrides.cmaketools,
-  },
+		init = function()
+			require("core.utils").load_mappings("cmaketools")
+		end,
+		opts = overrides.cmaketools,
+	},
 }
 
 return plugins
